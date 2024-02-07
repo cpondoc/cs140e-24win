0cc2554711e867b85d6729d02ed3e3fa76a84806
cpondoc <chris.pondoc@comcast.net>
2024-02-06 20:20:13 -0800

    Finish up first 2 parts

--- labs/9-device-int/1-vector-base/vector-base.h
+++ labs/9-device-int/1-vector-base/vector-base.h
@@ -13,13 +13,26 @@
  */
 
 // return the current value vector base is set to.
+cp_asm_set(vector_base_asm, p15, 0, c12, c0, 0)
+cp_asm_get(vector_base_asm, p15, 0, c12, c0, 0)
+
 static inline void *vector_base_get(void) {
-    todo("implement using inline assembly to get the vec base reg");
+    // Use the fact that cp_asm_get defines the inline assembly?
+    return (void *)vector_base_asm_get();
 }
 
 // check that not null and alignment is good.
 static inline int vector_base_chk(void *vector_base) {
-    todo("check that not null and alignment is correct.");
+    // First, check that the vector is not null
+    if (!vector_base) {
+        return 0;
+    }
+
+    // Then, check that the alignment is correct -- lower 5 bits should be 0
+    uint32_t v = (uint32_t)vector_base;
+    if(bits_get(v, 0, 4) != 0) {
+        return 0;
+    }
     return 1;
 }
 
@@ -36,7 +49,8 @@ static inline void vector_base_set(void *vec) {
     if(v) 
         panic("vector base register already set=%p\n", v);
 
-    todo("set vector base here.");
+    // Set vector base here
+    vector_base_asm_set((uint32_t)vec);
 
     // make sure it equals <vec>
     v = vector_base_get();
@@ -53,7 +67,9 @@ vector_base_reset(void *vec) {
     if(!vector_base_chk(vec))
         panic("illegal vector base %p\n", vec);
 
-    todo("validate <vec> and set as vector base\n");
+   // Retrieve the old base vector, set the new vector, then validate
+   old_vec = vector_base_get();
+   vector_base_asm_set((uint32_t)vec);
 
     assert(vector_base_get() == vec);
     return old_vec;
--- labs/9-device-int/2-gpio-int/gpio-int.c
+++ labs/9-device-int/2-gpio-int/gpio-int.c
@@ -1,13 +1,28 @@
 // engler, cs140 put your gpio-int implementations in here.
 #include "rpi.h"
 
+// Enums for the address
+enum {
+    BASE = 0x20000000,
+    IRQ_TWO_PENDING = BASE + 0xB208,
+    RISING_EDGE = BASE + 0x20004C,
+    FALLING_EDGE = BASE + 0x200058,
+    ENABLE_IRQS = BASE + 0xB214,
+    EVENT_DETECTED = BASE + 0x200040,
+};
+
 // returns 1 if there is currently a GPIO_INT0 interrupt, 
 // 0 otherwise.
 //
 // note: we can only get interrupts for <GPIO_INT0> since the
 // (the other pins are inaccessible for external devices).
 int gpio_has_interrupt(void) {
-    todo("implement: is there a GPIO_INT0 interrupt?\n");
+    // Page 117 -- if enabled, we know the interrupt can be pending
+    uint32_t value = GET32(IRQ_TWO_PENDING);
+
+    // We want to get bit 17
+    value = value >> 17;
+    return DEV_VAL32(value & 0b1);
 }
 
 // p97 set to detect rising edge (0->1) on <pin>.
@@ -16,7 +31,25 @@ int gpio_has_interrupt(void) {
 // *after* a 1 reading has been sampled twice, so there will be delay.
 // if you want lower latency, you should us async rising edge (p99)
 void gpio_int_rising_edge(unsigned pin) {
-    todo("implement: detect rising edge\n");
+    // Check for correct pin
+    if (pin >= 32) {
+        panic("Incorrect -- pin is not accessible!");
+    }
+
+    // Read in detected event
+    dev_barrier();
+    uint32_t value = GET32(RISING_EDGE);
+    uint32_t mask = 1 << pin;
+
+    // Set the pin to 1 to clear, and then put back into the register value
+    value = value | mask;
+    PUT32(RISING_EDGE, value);
+    dev_barrier();
+
+    // Enable interrupts
+    value = 1 << 17;
+    PUT32(ENABLE_IRQS, value);
+    dev_barrier();
 }
 
 // p98: detect falling edge (1->0).  sampled using the system clock.  
@@ -25,17 +58,58 @@ void gpio_int_rising_edge(unsigned pin) {
 // interrupt is delayed two clock cycles.   if you want  lower latency,
 // you should use async falling edge. (p99)
 void gpio_int_falling_edge(unsigned pin) {
-    todo("implement: detect falling edge\n");
+    // Check for correct pin
+    if (pin >= 32) {
+        panic("Incorrect -- pin is not accessible!");
+    }
+
+    // Read in detected event
+    dev_barrier();
+    uint32_t value = GET32(FALLING_EDGE);
+    uint32_t mask = 1 << pin;
+
+    // Set the pin to 1 to clear, and then put back into the register value
+    value = value | mask;
+    PUT32(FALLING_EDGE, value);
+    dev_barrier();
+
+    // Enable interrupts
+    value = 1 << 17;
+    PUT32(ENABLE_IRQS, value);
+    dev_barrier();
 }
 
 // p96: a 1<<pin is set in EVENT_DETECT if <pin> triggered an interrupt.
 // if you configure multiple events to lead to interrupts, you will have to 
 // read the pin to determine which caused it.
 int gpio_event_detected(unsigned pin) {
-    todo("implement: is an event detected?\n");
+    // Check for correct pin
+    if (pin >= 32) {
+        panic("Incorrect -- pin is not accessible!");
+    }
+    
+    // Detect an event
+    dev_barrier();
+    uint32_t value = GET32(EVENT_DETECTED);
+    uint32_t mask = 1 << pin;
+
+    // Return if the bit at the pin offset is greater than 0
+    dev_barrier();
+    return DEV_VAL32((value & mask) > 0);
 }
 
 // p96: have to write a 1 to the pin to clear the event.
 void gpio_event_clear(unsigned pin) {
-    todo("implement: clear event on <pin>\n");
+    // Check for correct pin
+    if (pin >= 32) {
+        return;
+    }
+    
+    // Read in detected event
+    dev_barrier();
+    uint32_t value = 1 << pin;
+
+    // Set the pin to 1 to clear, and then put back into the register value
+    PUT32(EVENT_DETECTED, value);
+    dev_barrier();
 }

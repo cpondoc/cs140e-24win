5ed0b64473359473acd9499a38d6b101aac56968
cpondoc <chris.pondoc@comcast.net>
2024-02-27 21:44:00 -0800

    Finish part 2

--- labs/15-pinned-vm/code/pinned-vm.c
+++ labs/15-pinned-vm/code/pinned-vm.c
@@ -9,10 +9,28 @@
 #include "mmu.h"
 #include "procmap.h"
 
+// from last lab.
+#include "switchto.h"
+#include "full-except.h"
+
 // generate the _get and _set methods.
 // (see asm-helpers.h for the cp_asm macro 
 // definition)
 // arm1176.pdf: 3-149
+
+enum { OneMB = 1024*1024};
+
+// For the lockdown helpers
+cp_asm(lockdown_index, p15, 5, c15, c4, 2);
+cp_asm(lockdown_va, p15, 5, c15, c5, 2);
+cp_asm(lockdown_pa, p15, 5, c15, c6, 2);
+cp_asm(lockdown_attr, p15, 5, c15, c7, 2);
+
+// For the xlate pa
+cp_asm(xlate_pa, p15, 0, c7, c4, 0);
+
+// For TLB contains
+cp_asm_set(xlate_kern_wr, p15, 0, c7, c8, 1);
 
 // define the following routines.
 #if 0
@@ -48,13 +66,49 @@ uint32_t xlate_pa_get(void);
 
 #endif
 
+// used to store the illegal address we will write.
+static volatile uint32_t illegal_addr;
+
+
+// a trivial fault handler that checks that we got the fault
+// we expected.
+static void fault_handler(regs_t *r) {
+    uint32_t fault_addr;
+
+    // b4-44
+    asm volatile("MRC p15, 0, %0, c6, c0, 0" : "=r" (fault_addr));
+
+    // make sure we faulted on the address that should be accessed.
+    if(fault_addr != illegal_addr)
+        panic("illegal fault!  expected %x, got %x\n",
+            illegal_addr, fault_addr);
+    else
+        trace("SUCCESS!: got a fault on address=%x\n", fault_addr);
+
+    // done with test.
+    trace("all done: going to reboot\n");
+    clean_reboot();
+}
+
+
 // do a manual translation in tlb:
 //   1. store result in <result>
 //   2. return 1 if entry exists, 0 otherwise.
 int tlb_contains_va(uint32_t *result, uint32_t va) {
     // 3-79
     assert(bits_get(va, 0,2) == 0);
-    return staff_tlb_contains_va(result, va);
+
+    // Set the value or the virtual address
+    xlate_kern_wr_set(va);
+
+    // Read the value of the PA register
+    uint32_t pa_register = xlate_pa_get();
+
+    // Check the 10-31st bits, since that contains the PA if the translation was successful
+    *result = ((bits_get(pa_register, 10, 31)) << 10) + bits_get(va, 0, 9);
+
+    // Check the value of the 0th bit -- if 0, then it is successful
+    return bit_is_off(pa_register, 0);
 }
 
 // map <va>-><pa> at TLB index <idx> with attributes <e>
@@ -63,17 +117,15 @@ void pin_mmu_sec(unsigned idx,
                 uint32_t pa,
                 pin_t e) {
 
-    staff_pin_mmu_sec(idx, va, pa, e);
-    return;
 
     demand(idx < 8, lockdown index too large);
     // lower 20 bits should be 0.
     demand(bits_get(va, 0, 19) == 0, only handling 1MB sections);
     demand(bits_get(pa, 0, 19) == 0, only handling 1MB sections);
 
-    if(va != pa)
+    /* if(va != pa)
         panic("for today's lab, va (%x) should equal pa (%x)\n",
-                va,pa);
+                va,pa); */
 
     debug("about to map %x->%x\n", va,pa);
 
@@ -81,10 +133,23 @@ void pin_mmu_sec(unsigned idx,
     // these will hold the values you assign for the tlb entries.
     uint32_t x, va_ent, pa_ent, attr;
 
-    // put your code here.
-    unimplemented();
+    // 1. Select the specific lockdown entry in the TLB Lockdown Index Register
+    x = idx;
+    lockdown_index_set(x);
 
-#if 0
+    // 2. Select the virtual address and its options in the TLB Lockdown VA Register
+    va_ent = va + (e.G << 9) + e.asid;
+    lockdown_va_set(va_ent);
+
+    // 3. Configure specific characteristics of this mapping in the TLB Lockdown Attributes Register
+    attr = (e.mem_attr << 1) + (e.dom << 7);
+    lockdown_attr_set(attr); 
+
+    // 4. Select the physical address and its options in the TLB Lockdown PA Register
+    pa_ent = (e.AP_perm << 1) + (e.pagesize << 6) + pa + 1;
+    lockdown_pa_set(pa_ent);
+
+//#if 0
     // put this back in when defined.
     if((x = lockdown_va_get()) != va_ent)
         panic("lockdown va: expected %x, have %x\n", va_ent,x);
@@ -92,7 +157,7 @@ void pin_mmu_sec(unsigned idx,
         panic("lockdown pa: expected %x, have %x\n", pa_ent,x);
     if((x = lockdown_attr_get()) != attr)
         panic("lockdown attr: expected %x, have %x\n", attr,x);
-#endif
+//#endif
 }
 
 // check that <va> is pinned.  
@@ -130,8 +195,27 @@ static void *null_pt = 0;
 
 // fill this in based on the test code.
 void pin_mmu_init(uint32_t domain_reg) {
-    staff_pin_mmu_init(domain_reg);
-    return;
+    // if we are correct this will never get accessed.
+    // since all valid entries are pinned.
+    null_pt = kmalloc_aligned(4096*4, 1<<14);
+    assert((uint32_t)null_pt % (1<<14) == 0);
+
+    // initialize everything, after bootup.
+    // <mmu.h>
+    staff_mmu_init();
+
+    // ******************************************************
+    // 4. setup vm hardware.
+    //  - page table, asid, pid.
+    //  - domain permissions.
+
+    // b4-42: give permissions for all domains.
+
+    // Q3: if you set this to ~0, what happens w.r.t. Q1?
+    staff_domain_access_ctrl_set(domain_reg); 
+
+    //staff_pin_mmu_init(domain_reg);
+    //return;
 }
 
 void pin_mmu_switch(uint32_t pid, uint32_t asid) {

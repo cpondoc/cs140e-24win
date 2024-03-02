a03b792c746d8e10dd0469ab64584adc19cae3f3
cpondoc <chris.pondoc@comcast.net>
2024-03-01 17:02:32 -0800

    Finish part 2

--- labs/16-vm-page-table/code/pt-vm.c
+++ labs/16-vm-page-table/code/pt-vm.c
@@ -2,6 +2,7 @@
 #include "pt-vm.h"
 #include "helper-macros.h"
 #include "procmap.h"
+#include "libc/bit-support.h"
 
 // turn this off if you don't want all the debug output.
 enum { verbose_p = 1 };
@@ -13,8 +14,9 @@ vm_pt_t *vm_pt_alloc(unsigned n) {
     vm_pt_t *pt = 0;
     unsigned nbytes = 4096 * sizeof *pt;
 
-    // allocate pt with n entries.
-    pt = staff_vm_pt_alloc(n);
+    // if we are correct this will never get accessed.
+    // since all valid entries are pinned.
+    pt = kmalloc_aligned(nbytes, 1<<14);
 
     demand(is_aligned_ptr(pt, 1<<14), must be 14-bit aligned!);
     return pt;
@@ -72,8 +74,23 @@ vm_map_sec(vm_pt_t *pt, uint32_t va, uint32_t pa, pin_t attr)
     assert(index < PT_LEVEL1_N);
 
     vm_pte_t *pte = 0;
-    return staff_vm_map_sec(pt,va,pa,attr);
 
+    // Use first table index from the virtual address to index into pt
+    pte = &pt[index];
+
+    // Start setting the fields
+    pte->tag = 0b10;
+    pte->sec_base_addr = pa >> 20;
+    pte->B = bit_get(attr.mem_attr, 0);
+    pte->C = bit_get(attr.mem_attr, 1);
+    pte->XN = pt->XN;
+    pte->domain = attr.dom;
+    pte->IMP = pt->IMP;
+    pte->AP = bits_get(attr.AP_perm, 0, 2);
+    pte->TEX = bits_get(attr.mem_attr, 2, 4);
+    pte->APX = bit_get(attr.AP_perm, 3);
+    pte->S = pt->S;
+    pte->nG = !attr.G;
 
     if(verbose_p)
         vm_pte_print(pt,pte);
@@ -84,7 +101,15 @@ vm_map_sec(vm_pt_t *pt, uint32_t va, uint32_t pa, pin_t attr)
 // lookup 32-bit address va in pt and return the pte
 // if it exists, 0 otherwise.
 vm_pte_t * vm_lookup(vm_pt_t *pt, uint32_t va) {
-    return staff_vm_lookup(pt,va);
+    // Get the entry in the page table array
+    unsigned index = va >> 20;
+    vm_pte_t *pte = &pt[index];
+
+    // Check if tag is non-zero
+    if (pte->tag == 0b10) {
+        return pte;
+    }
+    return 0;
 }
 
 // manually translate <va> in page table <pt>
@@ -94,7 +119,15 @@ vm_pte_t * vm_lookup(vm_pt_t *pt, uint32_t va) {
 //
 // NOTE: we can't just return the result b/c page 0 could be mapped.
 vm_pte_t *vm_xlate(uint32_t *pa, vm_pt_t *pt, uint32_t va) {
-    return staff_vm_xlate(pa,pt,va);
+    // Check 0
+    vm_pte_t *pte = vm_lookup(pt, va);
+    if (pte == 0) {
+        return 0;
+    }
+
+    // Translate the physical address
+    *pa = (pte->sec_base_addr << 20) | (bits_get(va, 0, 19));     
+    return pte;
 }
 
 // compute the default attribute for each type.
@@ -127,8 +160,38 @@ vm_pt_t *vm_map_kernel(procmap_t *p, int enable_p) {
 
     vm_pt_t *pt = 0;
 
-    return staff_vm_map_kernel(p,enable_p);
+    // Compute all the domain permissions
+    uint32_t d = dom_perm(p->dom_ids, DOM_client);
 
+    // Initialize the MMU
+    vm_mmu_init(d);
+    
+    // Allocate the page table
+    pt = vm_pt_alloc(PT_LEVEL1_N);
+
+    // Switch after the alloc
+    vm_mmu_switch(pt, kern_pid, kern_asid);
+
+    // Iterate through each of the entries
+    for (int i = 0; i < p->n; i++) {
+        pin_t pin = attr_mk(&p->map[i]);
+        vm_map_sec(pt, p->map[i].addr, p->map[i].addr, pin);
+    }
+
+    // Enable the mmu if enable_p is set.
+    if (enable_p) {
+        vm_mmu_enable();
+    }
+
+    // Check with vm_lookup
+    for (int i = 0; i < p->n; i++) {
+        // vm_lookup(vm_pt_t *pt, uint32_t va)
+        if (!vm_lookup(pt, p->map[i].addr)) {
+            panic("Doesn't exist!\n");
+        }
+    }
+
+    // Assert and check
     assert(pt);
     return pt;
 }
